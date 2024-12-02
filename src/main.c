cb532d38eed5c83d6b5f81346a9b2d427607002a
AlexKM57 <komcinclair@gmail.com>
2024-12-02 03:36:01 +0100

    espace

--- src/main.c
+++ src/main.c
@@ -1,11 +1,8 @@
 #include <SDL.h>
 #include <stdio.h>
 
-
-
 //gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
 // ligne pour compiler
-
 
 int main(int argc, char  **argv)
 {
@@ -13,8 +10,6 @@ int main(int argc, char  **argv)
 	SDL_VERSION(&nd);
 
 	printf("Bienvenue sur la SDL %d.%d.%d !\n", nd.major, nd.minor, nd.patch);
-
-
 
 	return 0;
 }
