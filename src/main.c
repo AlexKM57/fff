9d76e4a07d7ed05d53eefb0844cde238a9c01e70
AlexKM57 <komcinclair@gmail.com>
2024-12-02 03:25:14 +0100

    debut

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <SDL.h>
+#include <stdio.h>
+
+
+
+//gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
+
+
+int main(int argc, char  **argv)
+{
+	SDL_version nd;
+	SDL_VERSION(&nd);
+
+	printf("Bienvenue sur la SDL %d.%d.%d !\n", nd.major, nd.minor, nd.patch);
+
+
+
+	return 0;
+}
